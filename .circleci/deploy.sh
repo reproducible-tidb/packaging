bfbe0745ae167c44178275d2ed209b23ac662aec
HikawaRin <HikawaRin@outlook.com>
2022-01-07 06:38:38 -0500

    feat: add circle ci

---
+++ .circleci/deploy.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+echo $1
+git config --global user.email "circle@ci.com"
+git config --global user.name "CI"
+
+git checkout -b circle_ci_release
+git remote add release https://${1}@github.com/HikawaRin/TBD.git
+
+git add results
+git commit -am "Circle CI: Deploy"
+
+git push --force release
---
+++ .circleci/install_TBD.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+git clone https://github.com/reproducible-tidb/TBD.git ../TBD
