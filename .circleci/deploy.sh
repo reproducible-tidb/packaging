40321949f9fdb6798e1d3c169e9a0dc33754c09d
HikawaRin <HikawaRin@outlook.com>
2022-01-07 06:57:00 -0500

    fix: direct result to group

--- .circleci/deploy.sh
+++ .circleci/deploy.sh
@@ -4,7 +4,7 @@ git config --global user.email "circle@ci.com"
 git config --global user.name "CI"
 
 git checkout -b circle_ci_release
-git remote add release https://${1}@github.com/HikawaRin/TBD.git
+git remote add release https://${1}@github.com/reproducible-tidb/packaging.git
 
 git add results
 git commit -am "Circle CI: Deploy"
