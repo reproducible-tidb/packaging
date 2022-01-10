e01c341c78df4ec90dd7a9f6220e122587d7d44e
Allen Zhong <allen@moe.cat>
2022-01-10 19:35:13 +0800

    adjust script

--- .drone/deploy.sh
+++ .drone/deploy.sh
@@ -1,15 +1,14 @@
 #!/bin/bash
 set -x
 
-git config --global user.email "runner@drone.zhoal.pw"
-git config --global user.name "Drone CI"
-
 git checkout -b results-drone
 git remote add release $GIT_REMOTE
+
+git config --global user.email "runner@drone.zhoal.pw"
+git config --global user.name "Drone CI"
 
 git add results
 git commit -m "Drone CI: Update build results"
 
-wc -l /root/.ssh/id_rsa
 git push --force release
 
